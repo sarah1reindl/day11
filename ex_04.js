7fc8a6368914af94039e9d21d5346bc1f04b7e80
sarah1reindl <sarah1.reindl@epitech.digital>
2024-10-14 11:44:10 +0200

    ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,10 @@
+function initializeMachine(machineName, timeRequired) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`${machineName} initialized`); 
+        }, timeRequired * 3000); 
+    });
+}
+initializeMachine("Computer", 3).then((value) => {
+    console.log(value); 
+});
