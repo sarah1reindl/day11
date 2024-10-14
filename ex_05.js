ef1295ff7bff19bd62eaa41919700c23485ab478
sarah1reindl <sarah1.reindl@epitech.digital>
2024-10-14 11:54:10 +0200

    ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,27 @@
+var seed = 0;
+
+function getID() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            var id = Math.sin(seed++) * 10000;
+            id = parseInt(id < 0 ? -id : id);
+            resolve(id);
+        }, 500);
+    });
+}
+
+// Do not modify what's above, write your code below
+// Don't forget to re-assign seed !
+
+
+
+function assignID(machineName, newSeed) {
+     seed = newSeed; 
+
+    return getID().then((id) => {
+        return `${machineName} : ${id}`; 
+    });
+}
+assignID("Oxygen filter", 1).then((value) => {
+    console.log(value); 
+});
